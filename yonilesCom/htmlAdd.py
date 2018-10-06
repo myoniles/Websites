5e27c3ccc1cc9c21ffe618733207d1f40001e5a7
Michael T Yoniles <BLMdnst8@gmail.com>
2018-10-05 23:52:59 -0400

    work on html add

--- yonilesCom/htmlAdd.py
+++ yonilesCom/htmlAdd.py
@@ -9,7 +9,9 @@ parser = ArgumentParser()
 parser.add_argument("htmlFile", help="The html file being posted to")
 parser.add_argument("postFile", help="The post file")
 parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", default=False)
-parser.add_argument("-f", "--formatted", action="store_true", dest="formatted", default=False, help="The postFile is already formatted html")
+parser.add_argument("-f", "--formatted", action="store_true", dest="formated", default=False, help="The postFile is already formatted html")
+parser.add_argument("-n", "--newFile", action="store", dest="newFileName", help="Specifies a filename to dump a new standalone html file")
+parser.add_argument("-t","--template", action="store", dest="template", default="template.html", help="If, \"-n\" option is selected, chooses the new template the html doc will be based on")
 args=parser.parse_args()
 
 htmlFile = open(args.htmlFile)
@@ -20,11 +22,20 @@ post_text = postFile.read()
 index = [ i for i in range(len(lines)) if ("Content Begins" in lines[i])]
 index = index[0]
 title = sys.argv[2].split(".")
+
+# make an assumption that if the filetype is html that the input is formated
+if title[1] == "html":
+	args.formated = True
 title = title[0]
 
 lines.insert(index+1, "<div class=\"content\" id=\""+ title +"\">\n")
 lines.insert(index+2, "<h2>"+ title + "</h2>\n")
-lines.insert(index+3, "<p>" + post_text + "</p>\n")
+if args.formated:
+	lines.insert(index+3, post_text + "\n")
+else:
+	# is it Jank?
+	# yes
+	lines.insert(index+3, "<p>" + post_text + "</p>\n")
 lines.insert(index+4, "<h6>" + str(datetime.datetime.today())[:10] + "</h6>\n")
 lines.insert(index+5, "</div>\n")
 
@@ -40,3 +51,8 @@ for l in lines:
 
 htmlFile.close()
 postFile.close()
+
+if args.newFileName != None:
+	with open(args.template) as fileTemplate:
+	# TODO dump into a new file
+		print(args.newFileName)
