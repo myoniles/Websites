0fd94483820ffeacdb4a9138daaa0cdee6c9f458
Michael T Yoniles <BLMdnst8@gmail.com>
2018-10-07 15:48:11 -0400

    reorganized, adding linking feature

--- yonilesCom/htmlAdd.py
+++ yonilesCom/htmlAdd.py
@@ -5,72 +5,67 @@ import sys
 import os.path
 from argparse import ArgumentParser
 
-parser = ArgumentParser()
-parser.add_argument("htmlFile", help="The html file being posted to")
-parser.add_argument("postFile", help="The post file")
-parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", default=False)
-parser.add_argument("-f", "--formatted", action="store_true", dest="formated", default=False, help="The postFile is already formatted html")
-parser.add_argument("-n", "--newFile", action="store", dest="newFileName", help="Specifies a filename to dump a new standalone html file")
-parser.add_argument("-t", "--title", action="store", dest="title", help="Specify the title of the post")
-parser.add_argument("--template", action="store", dest="template", default="template.html", help="If, \"-n\" option is selected, chooses the new template the html doc will be based on")
-args=parser.parse_args()
+def getArgs():
+	parser = ArgumentParser()
+	parser.add_argument("htmlFile", help="The html file being posted to")
+	parser.add_argument("postFile", help="The post file")
+	parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", default=False)
+	parser.add_argument("-f", "--formatted", action="store_true", dest="formatted", default=False, help="The postFile is already formatted html")
+	parser.add_argument("-n", "--newFile", action="store", dest="newFileName", help="Specifies a filename to dump a new standalone html file")
+	parser.add_argument("-s", "--standalone", action="store_true", dest="standalone", default=False, help="creates a standalone file in \"./post/\"")
+	parser.add_argument("-t", "--title", action="store", dest="title", help="Specify the title of the post")
+	parser.add_argument("--template", action="store", dest="template", default="template.html", help="If, \"-n\" option is selected, chooses the new template the html doc will be based on")
+	return parser.parse_args()
 
-htmlFile = open(args.htmlFile)
-postFile = open(args.postFile)
 
-lines = htmlFile.readlines()
-post_text = postFile.read()
-index = [ i for i in range(len(lines)) if ("Content Begins" in lines[i])]
-index = index[0]
+def get_post_text( to_post_file, formatted=False, title=None, standalone=None):
+	with open(to_post_file) as postFile:
+		lines = postFile.readlines()
+		if standalone:
+			lines.insert(0,"<h2> <a href=\"" + standalone + "\">"+ title + "</a></h2>\n")
+		else:
+			lines.insert(0,"<h2>"+ title + "</h2>\n")
+		lines.insert(0,"<div class=\"content\" id=\""+ title +"\">\n")
+		lines.append("<h6>" + str(datetime.datetime.today())[:10] + "</h6>\n")
+		lines.append("</div>\n")
+	return lines
 
-if (args.title != None):
-	title = sys.argv[2].split(".")
-	# make an assumption that if the filetype is html that the input is formated
-	if title[1] == "html":
-		args.formated = True
-	title = title[0]
-else:
+def get_new_text(post_text, newFileName):
+	with open(newFileName) as newFile:
+		lines = newFile.readlines()
+		index = [i for i in range(len(lines)) if ("Content Begins" in lines[i])]
+		index = index[0] +1
+		for line in post_text:
+			lines.insert(index, line)
+			index = index +1
+		return lines
+
+def paste_to(postText, htmlFileName):
+	with open(htmlFileName, "w") as htmlFile:
+		for line in postText:
+			htmlFile.write(line)
+
+def main():
+	args = getArgs()
 	title = args.title
+	if title == None:
+		title=args.postFile.split(".")[0]
+	cmdLineTitle = title.replace(" ", "_") + ".html"
 
-lines.insert(index+1, "<div class=\"content\" id=\""+ title +"\">\n")
-lines.insert(index+2, "<h2>"+ title + "</h2>\n")
-if args.formated:
-	lines.insert(index+3, post_text + "\n")
-else:
-	# is it Jank?
-	# yes
-	lines.insert(index+3, "<p>" + post_text + "</p>\n")
-lines.insert(index+4, "<h6>" + str(datetime.datetime.today())[:10] + "</h6>\n")
-lines.insert(index+5, "</div>\n")
+	postText = get_post_text(args.postFile, formatted = args.formatted, title=title, standalone=cmdLineTitle)
+	new_text= get_new_text(postText, args.htmlFile)
+	paste_to(new_text, args.htmlFile)
 
-htmlFile.close()
+	if args.verbose:
+		print(new_text)
 
-htmlFile = open(sys.argv[1], 'w')
+	if args.standalone:
+		new_text= get_new_text(postText, args.template)
+		if args.newFileName != None:
+			paste_to(new_text, args.newFileName)
+		else:
+			paste_to(new_text, "posts/" + cmdLineTitle)
+	return
 
-if args.verbose:
-	print(lines)
-
-for l in lines:
-	htmlFile.write(l)
-
-htmlFile.close()
-postFile.close()
-
-if args.newFileName != None:
-	with open(args.template) as fileTemplate:
-		with open(args.newFileName, "w+") as newFile:
-			lines = fileTemplate.readlines()
-			index = [ i for i in range(len(lines)) if ("Content Begins" in lines[i])]
-			index = index[0]
-			lines.insert(index+1, "<div class=\"content\" id=\""+ title +"\">\n")
-			lines.insert(index+2, "<h2>"+ title + "</h2>\n")
-			if args.formated:
-				lines.insert(index+3, post_text + "\n")
-			else:
-				# is it Jank?
-				# yes
-				lines.insert(index+3, "<p>" + post_text + "</p>\n")
-			lines.insert(index+4, "<h6>" + str(datetime.datetime.today())[:10] + "</h6>\n")
-			lines.insert(index+5, "</div>\n")
-			for l in lines:
-				newFile.write(l)
+if __name__ == "__main__":
+	main()
